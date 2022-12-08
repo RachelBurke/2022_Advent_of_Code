2fc26eaf4a0d45925f556f376fe496f9a3507da9
Rachel <97rachel.burke@gmail.com>
2022-12-08 14:50:19 -0500

    Day 1 Submission

--- Day_1/Day_1.ts
+++ Day_1/Day_1.ts
@@ -1,44 +1,40 @@
-const fs = require('fs');
-const util = require('util');
+const fs = require("fs");
+const util = require("util");
 
 const readFile = util.promisify(fs.readFile);
 
 function getCalorieData() {
-    return readFile('./Day_1_Input.txt', 'utf8');
+  return readFile("./Day_1_Input.txt", "utf8");
 }
 
 getCalorieData().then((data: string) => {
-    // Format Data
-    const calorieData = data.split("\r\n");
+  // Format Data
+  const calorieData = data.split("\r\n");
 
-    // Parse Data
-    let elfCalories: Array<number> = [];
-    let elf = 0;
-    calorieData.forEach((calorieCount) => {
-        if (calorieCount && !isNaN(parseInt(calorieCount))) {
-            if (isNaN(elfCalories[elf])) {
-                elfCalories.push(0);
-            }
-            elfCalories[elf] += parseInt(calorieCount);
-        } else {
-            elf++;
-        }
-    });
+  // Parse Data
+  let elfCalories: Array<number> = [];
+  let elf = 0;
+  calorieData.forEach((calorieCount) => {
+    if (calorieCount && !isNaN(parseInt(calorieCount))) {
+      if (isNaN(elfCalories[elf])) {
+        elfCalories.push(0);
+      }
+      elfCalories[elf] += parseInt(calorieCount);
+    } else {
+      elf++;
+    }
+  });
 
-    // Part 1
-    const largestCalories = Math.max(...elfCalories);
-    const elfLargestCalories = elfCalories.indexOf(largestCalories);
-    console.log("Elf " + elfLargestCalories + ": " + largestCalories);
+  // Part 1
+  const largestCalories = Math.max(...elfCalories);
+  const elfLargestCalories = elfCalories.indexOf(largestCalories);
+  console.log("Elf " + elfLargestCalories + ": " + largestCalories); //69281
 
-    // Sort Data
-    const sortedElfCalories = [...elfCalories].sort((a, b) => b - a);
-    sortedElfCalories.length = 3;
-    const top3Total = sortedElfCalories.reduce((a, b) => a + b, 0);
+  // Sort Data
+  const sortedElfCalories = [...elfCalories].sort((a, b) => b - a);
+  sortedElfCalories.length = 3;
+  const top3Total = sortedElfCalories.reduce((a, b) => a + b, 0);
 
-    //Part 2
-    sortedElfCalories.forEach(calories => {
-        const elfNumber = elfCalories.indexOf(calories);
-        console.log("Elf " + elfNumber + ": " + calories);
-    });
-    console.log(top3Total);
+  //Part 2
+  console.log("Top 3 Total Sum: " + top3Total); // 201524
 });
